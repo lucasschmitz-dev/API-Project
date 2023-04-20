5b840743cb33d4ca9a15eed73d184bd852ddc042
Lucas Schmitz <mrlucas.schmitz@aol.de>
2023-04-20 13:42:29 +0200

    temp(fix input test)

--- frontend/cypress/e2e/input.cy.ts
+++ frontend/cypress/e2e/input.cy.ts
@@ -13,6 +13,14 @@ describe("Input Test", () => {
   });
 
   it("Input Field", () => {
+    cy.intercept("http://127.0.0.1:3000/weatherdata/ranked", [
+      { id: 1, likes: null },
+    ]);
+    cy.intercept("http://127.0.0.1:3000/weatherdata/1", {
+      fixture: "weatherData.json",
+    });
+    cy.intercept("http://127.0.0.1:3000/weatherdata/like/1", "true");
+    cy.intercept("http://127.0.0.1:3000/weatherdata/dislike/1", "true");
     cy.visit("/");
     const name = "Giraffen sind modisch 1";
     cy.get("#input-0").should("exist");
@@ -28,6 +36,14 @@ describe("Input Test", () => {
   // Input Feld überprüfen, schauen ob es , aber wo finde ich den Input Feld als Code?
   // und wie mache ich das bei einer Anzeige?
   it("thumbs up should be green when Image liked", () => {
+    cy.intercept("http://127.0.0.1:3000/weatherdata/ranked", [
+      { id: 1, likes: null },
+    ]);
+    cy.intercept("http://127.0.0.1:3000/weatherdata/1", {
+      fixture: "weatherData.json",
+    });
+    cy.intercept("http://127.0.0.1:3000/weatherdata/like/1", "true");
+    cy.intercept("http://127.0.0.1:3000/weatherdata/dislike/1", "true");
     cy.visit("/data");
     cy.get(".mdi-thumb-up")
       .should("exist")
@@ -42,6 +58,14 @@ describe("Input Test", () => {
   });
 
   it("thumbs down should be red when Image liked", () => {
+    cy.intercept("http://127.0.0.1:3000/weatherdata/ranked", [
+      { id: 1, likes: null },
+    ]);
+    cy.intercept("http://127.0.0.1:3000/weatherdata/1", {
+      fixture: "weatherData.json",
+    });
+    cy.intercept("http://127.0.0.1:3000/weatherdata/like/1", "true");
+    cy.intercept("http://127.0.0.1:3000/weatherdata/dislike/1", "true");
     cy.visit("/data");
     cy.get(".mdi-thumb-down")
       .should("exist")
